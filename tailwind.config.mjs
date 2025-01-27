8d4c4f90fd0e5061f6fa0360332231fc6f3e444b
Dan <dan@mac.home>
2025-01-27 16:58:57 +0000

    site soft launch

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'primary': "#f3bae5",
+        'secondary': "#94c0e9",
       },
     },
   },
