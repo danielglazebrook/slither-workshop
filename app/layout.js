664f47ed4e7814fadf055e487e864a2cd44d0283
Dan <dan@Dans-MacBook-Air.local>
2025-04-20 18:43:05 +0100

    added GA

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-QM1Z29RH14"></script>
+      <script> 
+        window.dataLayer = window.dataLayer || []; function gtag() {
+          dataLayer.push(arguments)
+          } 
+          gtag('js', new Date()); 
+          gtag('config', 'G-QM1Z29RH14');</script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
